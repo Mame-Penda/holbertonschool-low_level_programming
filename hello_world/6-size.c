fa1bffc713a286a8b911650ef718697828122cd6
root <root@IMHOTEM.localdomain>
2024-10-21 19:16:12 +0200

    betty

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -10,7 +10,6 @@ int main(void)
 	long long int c;
 	char d;
 	float f;
-	
 	printf("Size of a char: %lu byte(s)\n",
 	printf("Size of an int: %lu byte(s)\n",
 	printf("Size of a long int: %lu byte(s)\n",
