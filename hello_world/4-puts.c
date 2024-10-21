f9998f1313c8324985bd15258b07c380a202b529
root <root@IMHOTEM.localdomain>
2024-10-21 15:03:56 +0200

    betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,11 @@
 # include <stdio.h>
-# include <stdio.h>
 
 
-/**
+   /**
  * main - prints " Programming is like building a multilingual puzzle
  *                puzzle , followed by a new line.
  *                
  * Return: Always 0.
- *
  */
 int main (void)
  {
