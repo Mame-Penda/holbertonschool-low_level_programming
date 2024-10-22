0225b5e537ac103a05bf466127361f17af0eae91
root <root@IMHOTEM.localdomain>
2024-10-22 16:03:22 +0200

    betty

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,7 +3,6 @@
 # include <time.h>
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -19,11 +18,11 @@ if (n % 10 > 5)
 }
 else if (n % 10 == 0)
 {
-	printf("Last digit of %d is %d and less than 6 and not 0\n", n, n % 10);
+	printf("Last digit of %d is %d and is 0\n", n, n % 10);
 }
 else
 {
-	printf("Last digit of %d is %d 0\n", n, n % 10);
+	printf("Last digit of %d is %d and less than 6 and not 0\n", n, n % 10);
 }
 return (0);
-	}
+}
