f9a32d3e0859e50c147a9970c38b0dd466acdcdd
root <root@IMHOTEM.localdomain>
2024-10-22 22:32:31 +0200

    betty

---
+++ variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+/**
+ * Return: Always (0)
+ */
+ int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; aphabet <= 'z'; alphabet++)
+		putchar(alphabet);
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+		putchar(alphabet);
+
+	putchar('\n');
+
+	return (0);
+}
