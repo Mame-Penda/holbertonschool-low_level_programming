05cb97770813d9de3476ce9dbbfa25fa118c5691
root <root@IMHOTEM.localdomain>
2024-10-22 12:03:26 +0200

    betty

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -6,15 +6,18 @@
 int main(void)
 {
 	int n;
-	if(n > 0);
-	printf("%d is positive\n",);
+	if (n > 0);
+	{
+	printf("%d is positive\n", n);
 
-	if else (n ==0);
-	printf("%d is zero\n",);
-
-	else (n < 0);
-	printf("%d is negative\n",);
-
-	return(0);
+	else if (n == 0);
+	{
+	printf("%d is zero\n", n);
+	}
+	else
+	{
+	printf("%d is negative\n", n);
+	}
+	return (0);
 
 }
