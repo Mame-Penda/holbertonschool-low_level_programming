7685d79a27401dca292cd6787d81ffad3f95ee13
root <root@IMHOTEM.localdomain>
2024-10-22 10:32:44 +0200

    betty

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+	if(n > 0);
+	printf("%d is positive\n",);
+
+	if else (n ==0);
+	printf("%d is zero\n",);
+
+	else (n < 0);
+	printf("%d is negative\n",);
+
+	return(0);
+
+}
