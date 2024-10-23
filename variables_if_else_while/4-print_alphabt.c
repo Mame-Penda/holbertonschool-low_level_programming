fc2e53903410af13cef096ad90ab4ef00ba08c65
root <root@IMHOTEM.localdomain>
2024-10-23 10:32:34 +0200

    betty

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabets
+		for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+		{
+			if (alphabets != 'e' && alphabets != 'q')
+				putchar (alphabets);
+		}
+
+	putchar('\n');
+
+	return (0);
+
+}
