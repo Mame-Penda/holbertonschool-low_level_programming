764c73f9d4c2139df80ddcbe3b441681291eb9c1
root <root@IMHOTEM.localdomain>
2024-10-24 15:01:21 +0200

    betty

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,19 +1,18 @@
-# include <main.h>
-# include <stdio.h>
+#include "main.h"
 /**
+ *
  * main - Entry putchar
  * Return: Always 0
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
 	_putchar('\n');
 
 
