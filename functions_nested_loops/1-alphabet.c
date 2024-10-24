c9d0e934c041e08f6e9408597a9a977c682e8076
root <root@IMHOTEM.localdomain>
2024-10-24 15:38:28 +0200

    betty

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <main.h>
+/**
+ * main - print alphabet
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	letter = ('a')
+
+	while (letter <= 'z')
+	{
+		ft_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+	return (0);
+}
