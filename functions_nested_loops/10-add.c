54ccf92ed93d69a56360c42506232f134fd6f39f
root <root@IMHOTEM.localdomain>
2024-10-25 11:03:28 +0200

    betty

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Adds two integers
+ * @num1: the first integers
+ * @num2: the second integers
+ *
+ * Return: the result of the addition
+ */
+int add(int num1, int num2)
+{
+	return (num1+ num2);
+
+}
