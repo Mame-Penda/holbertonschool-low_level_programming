9189b69ec806d6f45f23ac252f5e75688ad396b2
root <root@IMHOTEM.localdomain>
2024-10-28 12:12:05 +0100

    betty

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - cheks for a digit character
+ * @c: the character to be checked
+ *
+ * Return: 1 if character is digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c > '0' && c < '9')
+		return (1);
+	else
+		return (0);
+}
