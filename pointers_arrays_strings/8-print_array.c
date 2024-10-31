16f9b84f7178fbdad9062c410b0a242698309097
root <root@IMHOTEM.localdomain>
2024-10-31 15:55:00 +0100

    betty

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -8,10 +8,11 @@
 void print_array(int *a, int n)
 {
 	int index;
+
 	for (index = 0; index < n; index++)
 	{
 		printf("%d", a[index]);
-		if (index == n -1)
+		if (index == n - 1)
 			continue;
 		printf(", ");
 	}
