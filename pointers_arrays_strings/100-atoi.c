752270d83cf3ca57d0e8bb33716a03d17181465b
root <root@IMHOTEM.localdomain>
2024-11-03 00:42:43 +0100

    betty

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "stdio.h"
 /**
  * _atoi - changes a string to an integer
  * @s: the string to be changed
@@ -7,17 +8,17 @@
  */
 int _atoi(char *s)
 {
-	int i = 1;
-	unsigned int num = 0;
-	do; {
-		if (*s == '-')
-			i *= -1;
-		else if (*s >= '0' && *s <= '9')
-			num = num * 10 + (*s - '0');
-		else if (num > 0)
-			break;
-	while (*s++);
-	}
-	return (num * 1);
+int i = 1;
+unsigned int num = 0;
+do {
+if (*s == '-')
+i *= -1;
+else if (*s >= '0' && *s <= '9')
+num = num * 10 + (*s - '0');
+else if (num > 0)
+break;
+} while (*s++);
+return (num *i);
+
 
 }
