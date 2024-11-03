637d94eed41b351514b8e8e32766cf27b65a3a2c
root <root@IMHOTEM.localdomain>
2024-11-03 01:41:31 +0100

    betty

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -2,21 +2,21 @@
 #include "string.h"
 /**
  * _strcpy - _strcpy copies the string pointed to by src
- * src: source
+ * @src: source
  * @dest: The buffer pointed
  *Return: dest
  */
 char *_strcpy(char *dest, char *src)
 {
-	int inc = 0;
+int inc = 0;
 
-	while (*(src + inc) != '\0')
+while (*(src + inc) != '\0')
 
-	{
+{
 *(dest + inc) = *(src + inc);
 inc++;
-	}
-	*(dest + inc) = '\0';
-	return (dest);
+}
+*(dest + inc) = '\0';
+return (dest);
 
 }
