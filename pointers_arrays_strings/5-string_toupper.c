7ed748a3604747aca45a41af8f51e26139a2961e
root <root@IMHOTEM.localdomain>
2024-11-04 10:38:25 +0100

    betty

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ * @str: parameter
+ * return: returns a character
+*/
+char *string_toupper(char *)
+{
+int index = 0;
+while (str[index++])
+{
+if (str[index] >= 'a' && str[index] <= 'z')
+str[index] -= 32;
+}
+return (str);
+}
