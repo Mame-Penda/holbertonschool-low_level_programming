0971fd1ccf2faddfb264d6fe7df45c3073920b1d
root <root@IMHOTEM.localdomain>
2024-11-04 19:30:07 +0100

    betty

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -3,15 +3,16 @@
  *_strchr - Locates a character in a string
  *@s: The string to be searched
  *@c: The character to be located
+ *Return: pointer to locates
  */
 char *_strchr(char *s, char c)
 {
-	while (*s && *s != c)
-		s++;
-	{
-		if (*s == c)
-			return (s);
-		else
-			return ('\0');
-	}
+while (*s && *s != c)
+s++;
+{
+if (*s == c)
+return (s);
+else
+return ('\0');
+}
 }
