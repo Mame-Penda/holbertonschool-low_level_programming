eccefa7e6adb15fbc6f802436f66c9f4d4a0330f
root <root@IMHOTEM.localdomain>
2024-11-04 19:52:29 +0100

    betty

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,2 +1,29 @@
 #include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: the string to be searched
+ * @accept: the string to be measured
+ *
+ * Return: the number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int bytes = 0;
+int i;
+while (*s)
+{
+for (i = 0; accept[i]; index++)
+{
+if (*s == accept[i])
+{
+bytes++;
+break;
+}
+else if (accept[i + 1] == '\0')
+return (bytes);
+}
+s++;
+}
+return (bytes);
+}
 
