20a7f561ec73cebd0f331aef0baf8ccf3142f6ce
root <root@IMHOTEM.localdomain>
2024-11-05 22:16:59 +0100

    betty

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -13,11 +13,14 @@ long int sum2 = 0;
 
 for (i = 0; i < size; i++)
 {
-	sum1 += a[i * size + i];
+	sum1 += a[i];
+	a += size;
 }
+a -= size;
 for (i = 0; i < size; i++)
 {
-	sum2 += a[i * size + (size - 1 - i)];
+	sum2 += a[i];
+	a -= size;
 }
-printf(" %ld, %ld\n", sum1, sum2);
+printf("%ld, %ld\n", sum1, sum2);
 }
