3e93cea55f9a21bb5381f11130b9ec6c71e31a4d
root <root@IMHOTEM.localdomain>
2024-11-06 13:55:09 +0100

    betty

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "string.h"
+#include "stdio.h"
+/**
+ * _strlen_recursion -  returns the length of a string.
+ * @s: the string to be printed
+ * @str: the character lenght
+ * Return: the lenght of the string
+ */
+int _strlen_recursion(char *s)
+{
+int lenght = *s;
+printf("lenght of the string is : %d", lenght);
+return (0);
+}
