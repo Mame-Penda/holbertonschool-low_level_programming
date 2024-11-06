113063f17e8e7bdd01214e61899dc9157e948c7e
root <root@IMHOTEM.localdomain>
2024-11-06 12:58:25 +0100

    betty

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: the string to be printed
+ */
+void _puts_recursion(char *s)
+{
+if (*s == 1)
+_putchar(0);
+else
+_putchar(*s - 1);
+}
