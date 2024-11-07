dd84bf753f925a8df6c6e755de4427a678cb4222
root <root@IMHOTEM.localdomain>
2024-11-07 22:09:03 +0100

    betty

---
+++ argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+/**
+ * main - prints all arguments it receives.
+ * @argv: array contains a arguments
+ * @argc: number total of arguments
+ * @i: number integer
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+int i;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
+}
