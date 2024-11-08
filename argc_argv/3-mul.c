abdd38e751862c4bb7cb0517151ee8e50aeb92db
root <root@IMHOTEM.localdomain>
2024-11-08 18:58:44 +0100

    betty

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+#include "stdlib.h"
+/**
+ * main - multiplies two numbers.
+ * @argv: array conains a arguments
+ * @argc: the number total of arguments
+ * Returns: 0 always
+ */
+int main(int argc, char *argv[])
+{
+int i, j, mul;
+if (argc <= 2)
+{
+printf("Error\n");
+return (1);
+}
+int num1 = atoi(argv[1]);
+int num2 = atoi(argv[2]);
+{
+int num1 * num2
+printf("%d\n", mul);
+return (0);
+}
+}
