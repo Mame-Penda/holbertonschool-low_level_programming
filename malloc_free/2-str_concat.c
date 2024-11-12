5aca58c8ef12761a5a5c8857bf308f1747695162
root <root@IMHOTEM.localdomain>
2024-11-12 15:15:06 +0100

    betty

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -12,19 +12,37 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *concat_str;
-	int index, concat_index = 0, len = 0;
+	unsigned int len1 = 0, len2 = 0, i, j;
 
-	if (s1 == 0)
-		s1 = "";
-	if (s2 == 0)
-		s2 = "";
-	for (index = 0; s1[index] || s2[index]; index++)
-		len++;
-	concat_str = malloc(sizeof(char) * len);
+	if (s1 != 0)
+	{
+		while (s1[len1] != '\0')
+		{
+			len1++;
+		}
+	}
+
+	if (s2 != 0)
+	{
+		while (s2[len2] != '\0')
+		{
+			len2++;
+		}
+	}
+
+	concat_str = malloc((len1 + len2 + 1) * sizeof(char));
+
 	if (concat_str == 0)
-		for (index = 0; s1[index]; index++)
-			concat_str[concat_index++] = s1[index];
-	for (index = 0; s2[index]; index++)
-		concat_str[concat_index++] = s1[index];
+	{	
+		return (0);
+	}
+		for (i = 0; i < len1; i++)
+		{
+			concat_str[i] = s1[i];
+		}
+	for (j = 0; j < len2; j++)
+	{
+		concat_str[i + j] = s2[j];
+	}
 	return (concat_str);
 }
