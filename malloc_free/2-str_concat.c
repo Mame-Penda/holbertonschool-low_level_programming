5c783fe5aa63c0d09f7cfb639fd4eabf64d420bb
root <root@IMHOTEM.localdomain>
2024-11-12 12:33:36 +0100

    betty

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include "stdlib.h"
+#include "stdio.h"
+/*
+ * str_concat - concatenates two strings.
+ * @s1: the string to be concatenated upon
+ * @s2: the string to be concatenad to s1
+ * Return: if concatened is fail num=ll
+ *
+ */
+char *str_concat(char *s1, char *s2);
+{
+	int len1 = 0, len2 = 0, i, j;
+	char *cancat_str;
+	if (s1 == 0)
+		s1 = "";
+	if (s2 == 0)
+		s2 = "";
+	for (index = 0; s1[index]; || s2[index]; index++)
+		len++;
+	concat_str = malloc(sizeof(char) * len);
+	if (concat_str == 0)
+		for (index = 0; s1[index]; index++)
+			concat_str[concat_index++] = s1[index];
+	for (index = 0; s2[index]; index++)
+		concat_str[concat_index++] = s1[index];
+	return (concat_str);
+}
