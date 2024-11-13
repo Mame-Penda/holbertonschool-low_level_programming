07dfceff8109124c0750942f4dcbca56b8bcfe1e
root <root@IMHOTEM.localdomain>
2024-11-13 15:22:15 +0100

    betty

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include "stdlib.h"
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: the fisrt string
+ * @s2: the second string
+ * @n: is greater or equal to the length of s2 then use the entire string s2
+ *
+ * Return: NULL
+ *
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+char *string_nconcat
+unsigned int len = n, i;
+if (s1 == 0)
+return ("");
+if (s2 == 0)
+return ("");
+for (i = 0;s1[i]; i++)
+len++;
+concat = malloc(sizeof(char) * (len + 1));
+len = 0;
+for ( i =0;s1[i]; i++)
+concat[len++] = s1[i];
+for (i = 0; s2[i] && i < n; i++)
+concat[len++] = s2[i];
+concat[len] = '\0';
+return (concat);
+}
+
