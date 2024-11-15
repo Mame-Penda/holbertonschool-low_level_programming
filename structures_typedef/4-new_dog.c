f6dc445e81d62a635c1b8a9b982dd30b103ff5ff
root <root@IMHOTEM.localdomain>
2024-11-15 23:12:33 +0100

    betty

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,32 +11,34 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int name_lenght, owner_lenght, i;
-	dog_t *new_dog;
-	name_lenght = owner_lenght = 0;
-	while (name[name_lenght++]);
-	while (owner[owner_lenght++]);
-	new_dog = malloc(sizeof(dog_t));
-	if (new_dog == 0)
-		return (0);
-	new_dog->name = malloc(name_lenght * sizeof(new_dog->name));
-	if (new_dog->name == 0)
-	{
-		free(new_dog);
-		return (0);
-	}
-	for (i = 0; i < name_lenght; i++)
-		new_dog->name[i] = name[i];
-	new_dog->age = age;
-	new_dog->owner = malloc(owner_lenght * sizeof(new_dog->owner));
-	if (new_dog->owner == 0)
-	{
-		free(new_dog->name);
-		free(new_dog);
-		return (0);
-	}
-	for (i = 0; i < owner_lenght; i++)
-		new_dog->owner[i] = owner[i];
-	return (new_dog);
-}
+int name_lenght, owner_lenght, i;
+dog_t *new_dog;
+name_lenght = owner_lenght = 0;
 
+while (name[name_lenght++])
+	;
+while (owner[owner_lenght++])
+	;
+new_dog = malloc(sizeof(dog_t));
+if (new_dog == 0)
+return (0);
+new_dog->name = malloc(name_lenght * sizeof(new_dog->name));
+if (new_dog->name == 0)
+{
+free(new_dog);
+return (0);
+}
+for (i = 0; i < name_lenght; i++)
+new_dog->name[i] = name[i];
+new_dog->age = age;
+new_dog->owner = malloc(owner_lenght * sizeof(new_dog->owner));
+if (new_dog->owner == 0)
+{
+free(new_dog->name);
+free(new_dog);
+return (0);
+}
+for (i = 0; i < owner_lenght; i++)
+new_dog->owner[i] = owner[i];
+return (new_dog);
+}
