dde12673699dcc0875c7fe763c30731c42b7c8e2
root <root@IMHOTEM.localdomain>
2024-11-15 16:02:31 +0100

    betty

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,4 +1,5 @@
-#include "dog.h"
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - define a new type struct dog
  * @name: the name of the dog
@@ -11,4 +12,13 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
