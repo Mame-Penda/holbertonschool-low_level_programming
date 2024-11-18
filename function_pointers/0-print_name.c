11826d78b5b70cd5edc64c6de0a05f50bc1f941e
root <root@IMHOTEM.localdomain>
2024-11-18 13:50:18 +0100

    betty

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,13 +1,12 @@
 #include "function_pointers.h"
 #include "print_name.h"
 /**
- * Prints_name - write a function prints a name.
+ * print_name - prints a name  using the provided function
  * @name: the name to be printed
  * @f: a pointer to a function that prints a name
- *Return: 0
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != 0 && f != 0) 
+	if (name != 0 && f != 0)
 		f(name);
 }
