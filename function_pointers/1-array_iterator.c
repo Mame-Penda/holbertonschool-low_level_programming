f96c0a6caf8ed3556002b301274b1d7786642344
root <root@IMHOTEM.localdomain>
2024-11-19 14:55:06 +0100

    betty

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -8,11 +8,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array != 0 && action != 0 && size > 0)
-		return;
-	while (size-- > 0)
-	{
-		action(*array);
-		array++;
-	}
+if (array == NULL || action == NULL)
+return;
+while (size-- > 0)
+{
+action(*array);
+array++;
+}
 }
