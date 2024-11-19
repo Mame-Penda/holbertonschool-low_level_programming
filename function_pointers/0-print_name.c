3b44f8b20f07c87299122828e61fbfac3633e6a4
root <root@IMHOTEM.localdomain>
2024-11-19 09:55:54 +0100

    betty

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
  * print_name - prints a name  using the provided function
  * @name: the name to be printed
  * @f: a pointer to a function that prints a name
- * Return: nothing
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
