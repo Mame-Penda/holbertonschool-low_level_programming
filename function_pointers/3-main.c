ef50baf72744705f9e6932b20353db8f1f3d7065
root <root@IMHOTEM.localdomain>
2024-11-19 22:30:36 +0100

    betty

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -9,7 +9,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int (*oprt)(int, int);
+	int num1, num2;
+	char *op;
 
 
 	if (argc != 4)
@@ -18,12 +19,20 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	oprt = get_op_func(argv[2]);
-	if (!oprt)
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
+	if ((*op == '/' && num2 == 0) ||
+			(*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
 	return (0);
 }
