524f22cde9064fe3010cdcad83024c426b17a963
root <root@IMHOTEM.localdomain>
2024-11-20 12:33:50 +0100

    betty

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_numbers - prints numbers, followed by a new line.
+ * @n: the number of integers passed to the function
+ * @separator: the string to be printed
+ * @...: a variable numbers of parameters
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+		unsigned int i;
+	va_start(nums, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(nums, int));
+		
+		if (i != (n - 1) && separator != NULL)
+
+		printf("%s", séparator);
+	}
+	printf("\n");
+
+	va_end(nums);
+}
+
