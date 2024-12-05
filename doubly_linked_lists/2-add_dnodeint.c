bc2a6f9da5c85531395021491d55cd9d495331f2
Mame-Penda <mamependasadio1@gmail.com>
2024-12-05 11:21:15 +0100

    fixed addnode int

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,25 +10,23 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;
+dlistint_t *new_node;
 
-	new_node = malloc(sizeof(dlistint_t));
+new_node = malloc(sizeof(dlistint_t));
+if (!new_node || !head)
+return (NULL);
 
-	if (new_node == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	new_node->n = n;
-
-	if (*head != NULL)
-	{
-		(*head)->prev = new_node;
-
-		*head = new_node;
-	}
-	return (new_node);
+new_node->n = n;
+if (*head)
+{
+new_node->prev = NULL;
+new_node->next = *head;
+(*head)->prev = new_node;
+*head = new_node;
+return (new_node);
+}
+new_node->prev = NULL;
+new_node->next = NULL;
+*head = new_node;
+return (new_node);
 }
