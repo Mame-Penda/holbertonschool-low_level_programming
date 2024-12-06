52310e121acda50ebc8ede4d64699629f3411f20
Mame-Penda <mamependasadio1@gmail.com>
2024-12-06 21:44:58 +0100

    read textfile

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * read_textfile - a function that reads a text file and prints
+ * @filename: the filename to open
+ * @letters: the number of letters to read and print
+ * Return: the number of letters read and printed, or 0 on failure
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t len_r, len_w;
+	char *temp;
+
+	if (!filename || !letters)
+		return (0);
+
+
+	temp = malloc(sizeof(letters);
+			if (!temp)
+			return (0);
+
+			fd = open(filename, O_RDONLY);
+
+			if (fd == -1)
+			free(temp);
+			return (0);
+
+			len_r = read(fd, temp, letters);
+			if (len_r == -1)
+			fail_CLEANUP;
+
+			free(temp);
+			close(fd);
+			return (len_w);
+			}
