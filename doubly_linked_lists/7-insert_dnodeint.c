1a05a4ff23e330e812a63ced71ad7944fb28a174
Mame-Penda <mamependasadio1@gmail.com>
2024-12-06 15:17:42 +0100

    fixed insert note

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,19 +13,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node = NULL, *temp = NULL;
 	unsigned int i = 0;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (!new_node)
-		return (NULL);
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	new_node->n = n;
-
-	if (!h)
-		return (NULL);
 	if (idx == 0)
 		return (add_dnodeint(h, n));
-
-
+	temp = *h;
 	while (i < idx - 1)
 	{
 		if (!temp)
@@ -33,12 +23,17 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp = temp->next;
 		i++;
 	}
-
 	if (!temp->next)
 		return (add_dnodeint_end(h, n));
-	if  (temp->next)
-		temp->next->prev = new_node;
-	temp->next = new_node;
 
+	new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = prev_node->next;
+	new_node->prev = prev_node;
+	prev_node->next->prev = new_node;
+	prev_node->next = new_node;
 	return (new_node);
 }
