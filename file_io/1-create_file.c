5bc6d83eb23c5eb1650424efa7e887913e6d1bc8
Mame-Penda <mamependasadio1@gmail.com>
2024-12-06 22:28:52 +0100

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, PERMISSIONS);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, permissions);
 	if (fd == -1)
 		return (-1);
 	if (text_content)
