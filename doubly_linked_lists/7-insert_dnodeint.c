9032c6b116a7a97144a31ab7b6bdcc6a2ed80682
Mame-Penda <mamependasadio1@gmail.com>
2024-12-06 15:19:19 +0100

    fixed insert note

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -31,9 +31,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->next = prev_node->next;
-	new_node->prev = prev_node;
-	prev_node->next->prev = new_node;
-	prev_node->next = new_node;
+	new_node->next = temp->next;
+	new_node->prev = temp;
+	temp->next->prev = new_node;
+	temp->next = new_node;
 	return (new_node);
 }
