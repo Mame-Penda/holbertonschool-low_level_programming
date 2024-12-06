5ef4dcc56676f6cffba359f54c80fe28e93a27ea
Mame-Penda <mamependasadio1@gmail.com>
2024-12-06 14:16:55 +0100

    delete node

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -23,20 +23,16 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(temp);
 		return (1);
 	}
-	while (i < index)
+	while (temp && i < index)
 	{
-		if (!temp)
-			return (-1);
 		temp = temp->next;
 		i++;
 	}
-	if (!temp->next)
-	{
-		temp->prev->next = NULL;
-		free(temp);
-		return (1);
-	}
+	if (!temp)
+		return (-1);
+
 	temp->prev->next = temp->next;
 	temp->next->prev = temp->prev;
 	free(temp);
+	return (-1);
 }
